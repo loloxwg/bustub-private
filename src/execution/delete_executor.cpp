aab24e3b7cfdfcbd427786ffd74224fedc0545f0
Xwg <xuanwei.zhang@gatech.edu>
2023-07-19 19:50:47 +0800

    feat(execution): p3 delete operator

--- src/execution/delete_executor.cpp
+++ src/execution/delete_executor.cpp
@@ -18,10 +18,36 @@ namespace bustub {
 
 DeleteExecutor::DeleteExecutor(ExecutorContext *exec_ctx, const DeletePlanNode *plan,
                                std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx), plan_(plan), child_executor_(std::move(child_executor)) {
+  table_info_ = exec_ctx_->GetCatalog()->GetTable(plan_->TableOid());
+}
 
-void DeleteExecutor::Init() { throw NotImplementedException("DeleteExecutor is not implemented"); }
+void DeleteExecutor::Init() {}
 
-auto DeleteExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool { return false; }
+auto DeleteExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool {
+  if (is_success_) {
+    return false;
+  }
+  // record the number of deleted rows
+  int count = 0;
+  while (child_executor_->Next(tuple, rid)) {
+    if (table_info_->table_->MarkDelete(*rid, exec_ctx_->GetTransaction())) {
+      count++;
+      // update index
+      auto indexes = exec_ctx_->GetCatalog()->GetTableIndexes(table_info_->name_);
+      for (auto index : indexes) {
+        auto key =
+            tuple->KeyFromTuple(child_executor_->GetOutputSchema(), index->key_schema_, index->index_->GetKeyAttrs());
+        index->index_->DeleteEntry(key, *rid, exec_ctx_->GetTransaction());
+      }
+    }
+  }
+  // return the number of deleted rows
+  std::vector<Value> values;
+  values.emplace_back(INTEGER, count);
+  *tuple = Tuple(values, &plan_->OutputSchema());
+  is_success_ = true;
+  return true;
+}
 
 }  // namespace bustub
--- src/include/execution/executors/delete_executor.h
+++ src/include/execution/executors/delete_executor.h
@@ -60,5 +60,8 @@ class DeleteExecutor : public AbstractExecutor {
   const DeletePlanNode *plan_;
   /** The child executor from which RIDs for deleted tuples are pulled */
   std::unique_ptr<AbstractExecutor> child_executor_;
+  /** The table to delete from */
+  TableInfo *table_info_;
+  bool is_success_ = false;
 };
 }  // namespace bustub
